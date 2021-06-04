242ade07d826a2aeab895ebf38c42411f3e1a92c
“Jamie <knutsenjamie@yahoo.com>
2021-06-04 13:43:53 -0700

    add final edit, details and all delete methods for TreatsController

--- SweetSavory/Controllers/TreatsController.cs
+++ SweetSavory/Controllers/TreatsController.cs
@@ -69,5 +69,40 @@ namespace SweetSavory.Controllers
             ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorId");
             return View(thisTreat);
         }
+
+        [HttpPost]
+        public ActionResult Edit(Treat treat, int FlavorId)
+        {
+            if (FlavorId != 0) {
+            _db.FlavorTreats.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
+            }
+            _db.Entry(treat).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+            return View(thisTreat);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+            _db.Treats.Remove(thisTreat);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult DeleteFlavor(int joinId)
+        {
+            var joinEntry = _db.FlavorTreats.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+            _db.FlavorTreats.Remove(joinEntry);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
